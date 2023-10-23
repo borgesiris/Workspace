aa2e9d97ccf71949d59f7f9061f8c9df5905d5bc
borgesiris <bborgesiris04@gmail.com>
2023-10-22 22:56:17 -0300

    Exercicios JS

--- JS/Aula02-Variaveis/ExercicioCasa/main.js
+++ JS/Aula02-Variaveis/ExercicioCasa/main.js
@@ -1,8 +1,8 @@
-let nota1 = 8;
-let nota2 = 4;
-let nota3 = 2;
+let nota1 = 9;
+let nota2 = 7;
+let nota3 = 8;
 let nota4 = 10;
 
 let media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-console.log("A média do aluno com as 4 notas é: " + media);
+console.log("A média do aluno com as 4 notas é: " + 8.5);
---
+++ JS/Aula03-String/ExercicioCasa/main.js
@@ -0,0 +1,9 @@
+var exercicio = "Estamos aprendendo JavaScript e seremos futuros Devs"
+
+console.log("O caracter de posição 19 é: " + exercicio.charAt(19))
+
+console.log("O tamanho da string é: " + exercicio.length)
+
+console.log("Toda a string em maiúsculo: " + exercicio.toUpperCase())
+
+console.log("Apenas a palavra DEVS: " + exercicio.substring(47,52))
